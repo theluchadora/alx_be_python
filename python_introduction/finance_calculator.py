32f6219e45b635994010398433f3d0cb5c2e45e0
theluchadora <160585859+theluchadora@users.noreply.github.com>
2025-07-11 23:59:53 +0300

    Fix some spelling in task 5

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 monthly_income = int(input("Enter your monthly income: "))
-montly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
 
-monthly_savings = monthly_income - montly_expenses
+monthly_savings = monthly_income - monthly_expenses
 
 projected_savings = monthly_savings * 12 + ( monthly_savings * 12 * 0.05)
 

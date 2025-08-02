6f6e45e01834f6de0b9d3816dd6f3ca11f7907d1
theluchadora <160585859+theluchadora@users.noreply.github.com>
2025-08-02 16:50:24 +0300

    Completed task 2 and fixed some spelling

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -13,7 +13,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(0, 0), 0)
         self.assertEqual(self.calc.add(-5, -5), -10)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(-3, -3), 0)
         self.assertEqual(self.calc.subtract(0, 5), -5)
